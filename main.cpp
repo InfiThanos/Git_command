a86b05120a1990f352fade51157ce078283342ad
InfiThanos <max-vasilevv@yandex.ru>
2023-11-03 12:05:52 +0300

    Base

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char choise;
+    do
+    {
+        cout << "Calculator" << endl;
+        cout << "1 - sum" << endl
+             << "2 - sub" << endl
+             << "3 - mul" << endl
+             << "4 - div" << endl
+             << endl;
+
+        int result;
+
+        cin >> result;
+
+        switch (result)
+        {
+        case 1:
+            cout << "Function in development" << endl;
+            break;
+        case 2:
+            cout << "Function in development" << endl;
+            break;
+        case 3:
+            cout << "Function in development" << endl;
+            break;
+        case 4:
+            cout << "Function in development" << endl;
+            break;
+        default:
+            cout << "Input error" << endl;
+            break;
+        }
+
+        cout << "repeat? (y/n) ";
+        cin >> choise;
+        system("cls");
+    }
+    while(choise == 'y');
+
+    system("pause");
+
+    return 0;
+}
+
